1ff635f0d207fa773f7d744bb45a31a9d7fd3945
jjtellezruiz <jjtellezruiz@hotmail.com>
2021-06-10 08:15:00 +0200

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent  {
+
+  heroeBorrado: string = '';
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America']
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
