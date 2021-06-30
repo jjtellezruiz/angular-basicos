77c80ee9bf338dd1b5865bb592976a425e8b6e7f
jjtellezruiz <jjtellezruiz@hotmail.com>
2021-06-30 12:48:52 +0200

    mainPage component

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,9 +12,25 @@ interface Personaje {
 })
 export class MainPageComponent implements OnInit {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ]
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
+  }
+
+  nuevoEmpty: Personaje = {
+    nombre: '',
+    poder: 0
   }
 
   constructor() { }
@@ -23,7 +39,14 @@ export class MainPageComponent implements OnInit {
   }
 
   agregar(): void {   
-    console.log(this.nuevo)
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    };
   }
 
 }
